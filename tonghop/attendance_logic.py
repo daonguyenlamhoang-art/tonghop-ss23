814279082b4c58c5f4efccfadca4e32cfba96e73
daonguyenlamhoang-art <nguyenlonghoang302710@gmail.com>
2026-06-14 11:05:22 +0700

    Update attendance_logic.py

---
+++ tonghop/attendance_logic.py
@@ -0,0 +1,41 @@
+def clock_in(attendance_book):
+    employee_id = input("Nhập mã nhân viên: ")
+    employee_id = employee_id.upper()
+
+    for employee in attendance_book:
+        if employee["id"] == employee_id:
+            print("Mã nhân viên đã tồn tại.")
+            return
+
+    employee_name = input("Nhập tên nhân viên: ")
+    clock_in_time = input("Nhập giờ vào (HH:MM): ")
+
+    new_employee = {
+        "id": employee_id,
+        "name": employee_name,
+        "times": (
+            clock_in_time,
+            None
+        )
+    }
+    attendance_book.append(new_employee)
+    print("Thành công: Đã ghi nhận", employee_id, "chấm công vào lúc", clock_in_time
+    )
+
+
+def clock_out(attendance_book):
+    employee_id = input("Nhập mã nhân viên: ")
+    employee_id = employee_id.upper()
+
+    clock_out_time = input("Nhập giờ ra (HH:MM): ")
+
+    for employee in attendance_book:
+
+        if employee["id"] == employee_id:
+            old_clock_in = employee["times"][0]
+            employee["times"] = (old_clock_in, lock_out_time)
+
+            print("Đã cập nhật giờ ra.")
+            return
+
+    print("Không tìm thấy nhân viên.")
